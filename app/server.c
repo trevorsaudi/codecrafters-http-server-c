af6aa838938828f6164198e2fcb41b7733819563
trevorsaudi <isatrevor052@gmail.com>
2024-07-30 12:30:08 -0400

    pass stage

--- app/server.c
+++ app/server.c
@@ -12,16 +12,15 @@
 #define PORT 4221
 
 const char OK_msg[] = "HTTP/1.1 200 OK\r\n\r\n";
-const char NOTFOUND_msg[] = "HTTP/1.1 404 Not Found\r\n\r\n" ;
-int server_fd, connected_fd, request_fd;
-size_t OK_msg_length = sizeof(OK_msg);
-size_t NOTFOUND_msg_length = sizeof(OK_msg);
-void targetTokenizer(char str[], int connected_fd){
+const char NOTFOUND_msg[] = "HTTP/1.1 404 Not Found\r\n\r\n";
+int server_fd, connected_fd;
+
+void targetTokenizer(char str[], int connected_fd) {
     char *pch;
     char *target;
     pch = strtok(str, " ");
-    target = pch = strtok(NULL, " ");
-	if (target != NULL && strcmp(target, "/") == 0) {
+    target = strtok(NULL, " ");
+    if (target != NULL && strcmp(target, "/") == 0) {
         send(connected_fd, OK_msg, sizeof(OK_msg) - 1, MSG_CONFIRM);
     } else {
         send(connected_fd, NOTFOUND_msg, sizeof(NOTFOUND_msg) - 1, MSG_CONFIRM);
@@ -29,82 +28,79 @@ void targetTokenizer(char str[], int connected_fd){
 }
 
 int main() {
-	// Disable output buffering
-	setbuf(stdout, NULL);
- 	setbuf(stderr, NULL);
+    // Disable output buffering
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
-	
-	
-	struct sockaddr_in client_addr;
-	socklen_t client_addr_len;
-	client_addr_len = sizeof(client_addr);
-	
-	char recv_buf[BUFFER_SIZE];
-	size_t recv_buf_len;
-	recv_buf_len = sizeof(recv_buf);
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    printf("Logs from your program will appear here!\n");
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
 
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
-	
-	// Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(PORT),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
-	
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	printf("Waiting for a client to connect...\n");
+    char recv_buf[BUFFER_SIZE];
 
-	connected_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	if(connected_fd != -1){
-		printf("Client connected\n");
-		send(connected_fd, OK_msg, OK_msg_length, MSG_CONFIRM);
-	} 
-	
-	if (request_fd = recv(connected_fd, recv_buf, recv_buf_len , MSG_WAITALL) == -1){
-		printf("Error encountered when receiving data: ", strerror(errno));
-		close(server_fd);
-		return -1;
-	}else if (request_fd == 0){
-		printf("Client has closed the connection");
-		close(server_fd);
-		return 0;
-	}
-	targetTokenizer(recv_buf, request_fd);
-	
-	
-	
-	
-	close(server_fd);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        printf("Socket creation failed: %s...\n", strerror(errno));
+        return 1;
+    }
 
-	return 0;
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        printf("SO_REUSEADDR failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    struct sockaddr_in serv_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(PORT),
+        .sin_addr = { htonl(INADDR_ANY) },
+    };
+
+    if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+        printf("Bind failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        printf("Listen failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    printf("Waiting for a client to connect...\n");
+
+    connected_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    if (connected_fd == -1) {
+        printf("Accept failed: %s \n", strerror(errno));
+        close(server_fd);
+        return 1;
+    }
+
+    printf("Client connected\n");
+
+    while (1) {
+        memset(recv_buf, 0, BUFFER_SIZE);
+        ssize_t request_len = recv(connected_fd, recv_buf, BUFFER_SIZE - 1, 0); // Use 0 flag instead of MSG_WAITALL
+        if (request_len == -1) {
+            printf("Error encountered when receiving data: %s\n", strerror(errno));
+            close(connected_fd);
+            close(server_fd);
+            return -1;
+        } else if (request_len == 0) {
+            printf("Client has closed the connection\n");
+            break;
+        } else {
+            recv_buf[request_len] = '\0'; // Null-terminate the received string
+            targetTokenizer(recv_buf, connected_fd);
+        }
+    }
+
+    close(connected_fd);
+    close(server_fd);
+
+    return 0;
 }
-
-
-// we will write a parser that checks the http request, breaks it when it finds a carriage return, then populates a struct with the details 
-// we can then extract the URL from the request and parse the target
-
-
